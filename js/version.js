1da6b0170a038aa215594ce24d4a5dee707fdf8f
Paul-BarelyArtificial <paul@barelyartificial.com>
2026-07-03 13:00:21 +0100

    Versioning Fix Two

--- js/version.js
+++ js/version.js
@@ -1,2 +1,11 @@
-const APP_VERSION = "v0.1.2";
-const RELEASE_NAME = "Dark Brand Styling";
+/*
+==================================================
+BA Portal
+Version : v0.1.3
+Release : Centralised Version Management
+Date    : 03 July 2026
+==================================================
+*/
+
+const APP_VERSION = "v0.1.3";
+const RELEASE_NAME = "Centralised Version Management";
