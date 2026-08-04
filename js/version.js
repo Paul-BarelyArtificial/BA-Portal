8aad18bf82c2fae2cebec75a13d32db4beab3d03
Paul-BarelyArtificial <paul@barelyartificial.com>
2026-08-04 17:36:53 +0100

    UI Changes

--- js/version.js
+++ js/version.js
@@ -1,3 +1,3 @@
-/* BA Portal v0.4.0 */
-const APP_VERSION = "v0.4.0";
-const RELEASE_NAME = "Dashboard Widgets";
+/* BA Portal v0.4.1 */
+const APP_VERSION = "v0.4.1";
+const RELEASE_NAME = "Suggestion Form Fix";
